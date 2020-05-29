fce7c2c219407b36270cb89a9eb2012045315564
develup8 <develup8@gmail.com>
2020-05-29 22:11:56 +0900

    AND 연산자(&&)를 사용한 조건부 렌더링 리액트에서 false를 렌더링할때는 null과 마찬가지로 아무것도 나타나지 않는다.

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -2,11 +2,7 @@ import React, { Fragment } from 'react';
 
 function App() {
   const name = 'react';
-  return (
-    <>
-      {name === 'react' ? (<h1>hello react</h1>) : (<h1>not react</h1>)}
-    </>
-  );
+  return <div>{name === 'react' && <h1>hello react</h1>}</div>;
 }
 
 export default App;
