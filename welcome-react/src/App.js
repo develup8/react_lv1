891b5a72ee1c57ded6dffce1afe80eb8e87f7407
develup8 <develup8@gmail.com>
2020-06-01 00:16:04 +0900

    class 대신 className - JSX로 작성시 class 값을 설정해도 스타일이 적용은 되지만 브라우저의 개발자도에 console 탭에 경고가 나타납니다. (react v16이상부터 적용)

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   const name = 'react';
-  const style = {
-    backgroundColor : 'black',
-    color : 'aqua',
-    fontSize : '48px',
-    fotWeight : 'bold',
-    padding: 16 // 단위 생략시 자동으로 px 지정
-  }
-  return <div style={style}>{name}</div>
+  return <div className="react">{name}</div>
 }
 
 export default App;
