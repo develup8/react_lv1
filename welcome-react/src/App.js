ac6bdd3319ff8dcc1eda8adb4097c26a27f30aaf
develup8 <develup8@gmail.com>
2020-06-07 10:44:33 +0900

    태그 사이의 내용을 보여주는 children

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
