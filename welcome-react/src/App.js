ba77d2eb810190abe2feb101c27f651b469ee7cd
develup8 <develup8@gmail.com>
2020-06-07 11:34:13 +0900

    isRequired를 사용하여 필수 propTypes 설정 - propTypes를 지정하지 않을 때 경고 메세지를 띄워줌
    PropType 종류
    - https://github.com/facebook/prop-types 참고

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoriteNumber={8}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
