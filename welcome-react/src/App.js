d11ee4f1ec2845d912bc00da253e56999ec2b99a
develup8 <develup8@gmail.com>
2020-06-11 13:00:00 +0900

    state -컴포넌트 내부에서 바뀔 수 있는 값 - props는  부모 컴포넌트가설정하는 값이며, 컴포넌트자신은 읽기 전용으로만 사용 가능 - 클래스형 컴포넌트 state / 함수형 컴포넌트 useState
    # 클래스형컴포넌트의 state
    Line 4 ~ 10
    - 생성자 constructor 메서드로 state 설정
    - 반드시 super(props)를 호출해야 한다.
    - 현재 클래스형 컴포넌트가 상속하고있는 Component클래스가 지닌 생성자함수를호출해 준다.
    
    Line: 12 ~ 29
    - render 함수에서 state를 조회할때는 this.state를 조회하면된다.
    -이벤트 설정 : onClick이라는 값을 props로 넣어줘서 버튼이 클릭될 때 호출시킬 함수를 설정하게 해준다.
    
    # MyComponent.js 미사용 삭제

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 const App = () => {
-  return (
-    <MyComponent name="React" favoriteNumber={8}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
 };
 
 export default App;
