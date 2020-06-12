5f72b0c2fe22fa8e9523112ecbaf12dbf6fb4e85
develup8 <develup8@gmail.com>
2020-06-12 12:30:15 +0900

    state - state 객체 안에 여러 값이 있을 때 > setState 함수는 인자로 전달된 객체안에 들어있는값만 바꿔 줍니다.

--- welcome-react/src/Counter.js
+++ welcome-react/src/Counter.js
@@ -6,15 +6,17 @@ class Counter extends Component {
     // state의 초깃값 설정
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
 
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지않는값 : {fixedNumber}</h2>
         <button
           // onclick을 통해 버튼이 클릭되었을 때 호출할 함수 지정
           onClick={() => {
