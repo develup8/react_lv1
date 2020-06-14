68531d83ede5d75de345f4e2e4f296f34af61bea
develup8 <develup8@gmail.com>
2020-06-14 10:51:00 +0900

    함수형 컴포넌트에서 useState 사용하기 - useState 사용하기 > 함수형 useState 에서는 인자값의 형태는 자유이다. 숫자, 문자열, 객체, 배열 등등 > 함수를 호출하면 배열이 반환되고, 첫번째는 원소의 현재 상태, 두번째는 상태를 바꾸어주는 함수(Setter) 이다.

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-  return <Counter />;
+  return <Say />;
 };
 
 export default App;
