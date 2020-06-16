a8ad6af435e738b6bc38abdcaf0c3bdf8b87f84e
develup8 <develup8@gmail.com>
2020-06-16 12:45:40 +0900

    이벤트 핸들링 - 클래스형 컴포넌트 - 버튼을 누를 때 comment 값을 공백으로 설정 - button을 만들어 클릭 이벤트가 발생 -> 현재 comment 값을 메시지 박스로 띄운 후 -> comment 값을 공백으로 설정

--- welcome-react/src/EventPractice.js
+++ welcome-react/src/EventPractice.js
@@ -20,6 +20,16 @@ class EventPractice extends Component {
             });
           }}
         />
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({
+              message: "",
+            });
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
