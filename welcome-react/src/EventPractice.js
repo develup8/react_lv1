2d3201c839ed496494fdb62be52a0363c5ef98d9
develup8 <develup8@gmail.com>
2020-06-16 12:59:05 +0900

    임의 메서드 만들기 함수를 미리 준비하여 전달. 성능상으로는 차이가 거의 없으나, 가독성은 훨씬 높다. - 기본방식 > 클래스의 임의 메서드가 특정 HTML 요소의 이벤트로 등록되는 과정에서 메서드와 this의 관계가 끊어진다. - 컴포넌트 자신으로 가리키기 위해서는 메서드를 this와  바인딩하는 작업이 필요하다. (바인딩 하지않으면 undefined를 가리킴)

--- welcome-react/src/EventPractice.js
+++ welcome-react/src/EventPractice.js
@@ -5,6 +5,25 @@ class EventPractice extends Component {
     message: "",
   };
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
+
   render() {
     return (
       <div>
@@ -14,22 +33,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.value}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
