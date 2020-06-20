eca68b5c650e6a354bc56e3aa9fdb99c466df998
develup8 <develup8@gmail.com>
2020-06-20 10:51:57 +0900

    ref: DOM에 이름 달기 (클래스형 컴포넌트) - DOM에 직접 작업을 해야 할 때 사용. > input에서 onChange 이벤트가 발생 -> handeChange를 호출하여 state의 password를 업데이트 > button에서 onClick 이벤트가 발생 -> handleButtonClick을 호출하여 clicked 값을 true로 설정하고, validated 값을 검증

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
-import EventPractice from "./EventPractice";
+import React, { Component } from "react";
+import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 
-export default EventPractice;
+export default App;
