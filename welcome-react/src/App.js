330c0cfd210e2569036d1ea6c8ac9d70290365bc
develup8 <develup8@gmail.com>
2020-07-02 13:13:41 +0900

    컴포넌트 반복 자바스크립트 배열의 map() 함수 >기존 배열로 새로운 배열을 만드는 역할 > 문자열로 구성된 배열 선언 렌더링은 되지만 콘솔에 "key" prop이 없다는 경고 메시지가 표시된다.

--- welcome-react/src/App.js
+++ welcome-react/src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import InterationSample from "./InterationSample";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
-      </div>
-    );
+    return <InterationSample />;
   }
 }
 
---
+++ welcome-react/src/InterationSample.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const InterationSample = () => {
+  const names = ["눈사람", "얼음", "눈", "바람"];
+  const nameList = names.map((name) => <li>{name}</li>);
+
+  return <ul>{nameList}</ul>;
+};
+
+export default InterationSample;
