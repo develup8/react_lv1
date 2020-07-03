ce27cc9d6118dcde734309e51ec8b8670f8a6eca
develup8 <develup8@gmail.com>
2020-07-03 12:49:04 +0900

    동적 배열 렌더링 응용 - 데이터 추가 기능 구현하기 > 새로운 이름 등록 기능 구현 > input과 button을 렌더링 > 버튼을 클릭했을 때 호출할 onClick 함수를 선언하여 버튼의 onClick 이벤트로 설정 > onClick 함수에서는 배열의 내장 함수 concat을 사용하여 새 항목을 추가한 배열을 만들고, setNames를 통해 상태 업데이트 - contcat을 사용한 이유 > 배열 새항목 추가시 push대신 concat을 사용한 이유 > push는 기존 배열 자체를 변경 / concat은 새로운 배열을 생성 (불병성 유지)

--- welcome-react/src/InterationSample.js
+++ welcome-react/src/InterationSample.js
@@ -10,8 +10,25 @@ const InterationSample = () => {
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
 
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId값에 1을 더해준다
+    setNames(nextNames); // names 값을 업데이트 한다
+    setInputText(""); // inputText를 비운다
+  };
+
   const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
-  return <ul>{namesList}</ul>;
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default InterationSample;
