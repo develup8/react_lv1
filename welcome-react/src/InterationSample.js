00bc0bef0e047d6e96b58d6b3fb4d9aa513f409e
develup8 <develup8@gmail.com>
2020-07-03 12:30:25 +0900

    key - 컴포넌트 배열을 렌더링 했을 때 어떤 원소에 변동이 있었는지 알아내기 위해 사용 > key 가 없을 때는 Virtual DOM을 비교하는 과정에서 리스트를 순차적으로 비교하면서 감지한다 > key가 있으면 빠르게 알아낸다 - key 설정 > map함수의 인자로 전달되는 함수 내부에서 컴포넌트 props 설정하듯 설정 > 데이터가 가진 고윳값으로 설정 > 거유 번호가 없는 컴포넌트는 map 함수에 전달되는 콜백 함수의 인수인 index를 사용하면 된다.

--- welcome-react/src/InterationSample.js
+++ welcome-react/src/InterationSample.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const InterationSample = () => {
   const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name) => <li>{name}</li>);
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
 
   return <ul>{nameList}</ul>;
 };
