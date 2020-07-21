8912a2f41c1e15af77f8f659560c097d9c23f270
develup8 <develup8@gmail.com>
2020-07-21 23:29:41 +0900

    useEffect - 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook 이다. - 클래스형 컴포넌트의 componentDidMount, componentDidUpdate를 합친 형태

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        console.log('렌더링 완료!');
+        console.log({
+            name,
+            nickname
+        });
+    });
 
     const onChanngeName = e => {
         setName(e.target.value);
