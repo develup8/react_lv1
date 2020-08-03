01b14f90a1dcabf3d5660b3d441a857d5ff6e626
develup8 <develup8@gmail.com>
2020-08-03 12:47:29 +0900

    커스텀 Hooks 만들기 - 여러 컴포넌트에서 비슷한 기능을 공유할 경우 Hook를 만들면 된다. - 다른 개발자가 만든 hooks도 라이브러리로 설치하여 사용할 수 있다.

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Average from "./Average";
+import Info from "./Info";
 
 const App = () => {
-  return <Average />;
+  return <Info />;
 };
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,27 +1,18 @@
-import React, { useReducer } from "react";
-
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+import React from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "",
   });
   const { name, nickname } = state;
-  const onChannge = (e) => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
       <div>
-        <input name="name" value={name} onChange={onChannge} />
-        <input name="nickname" value={nickname} onChange={onChannge} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <p>
