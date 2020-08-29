39b6d39a9a52f0d25408120ead4d97ac3a751044
develup8 <develup8@gmail.com>
2020-08-30 00:43:37 +0900

    반응형 디자인 > media 쿼리 사용 > styled-component 매뉴얼에서 제공하는 유틸 함수 사용 > 참고 : https://styled-components.com/docs/advanced#media-templates

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -2,11 +2,30 @@ import React from "react";
 import styled, { css } from "styled-components";
 // Line 2: 단순 변수의 형태가 아니라 여러줄의 스타일 구문을 조건부로 설정해야 하는 경우에는 css를 불러와야 한다
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+// 위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 줍니다.
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props로 넣어 준 값을 직접 전달해 줄 수 있습니다 */
   background: ${(props) => props.color || "blue"};
   padding: 1em;
   display: flex;
+  margin: 0 autp;
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
