7fc5eeb165fead794fae27bb81aae94dfd6603a4
develup8 <develup8@gmail.com>
2020-09-29 06:12:48 +0900

    Route 하나에 여러 개의 path 설정하기 - 리액트 라우터 v5부터 적용된 기능이다.

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
     </div>
   );
 };
