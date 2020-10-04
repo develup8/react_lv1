b1492e9d91da71f8ec389adab9a11deb3dba054d
develup8 <develup8@gmail.com>
2020-10-04 12:33:18 +0900

    서브라우트 - 라우트 내부에 또 라우트를 정의하는 것이다. -  프로필 링크를 보여 주는 Profiles라는 라우트 컴포넌트를 만든다. - 그 안에 Profile 컴포넌트를 서브 라우트로 사용한다. - component 대신 render라는 props를 넣어 컴포넌트 자체를 전달하는 것이 아니라 보여주고 싶은 JSX를 넣어 준다. - 따로 컴포넌트를 만들기 애매한 상황이나, 컴포넌트에 props를 별도로 넣어 주고 싶을때 사용한다. - exact는 exact={true}와 같은 의미이다.

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 const App = () => {
   return (
@@ -15,16 +15,13 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/develup">develup 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
