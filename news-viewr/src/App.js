13759f87ce145d78259e65e525721e1f685b885b
develup8 <develup8@gmail.com>
2020-10-17 23:05:25 +0900

    뉴스 뷰어 UI 만들기 - styled-components를 사용하여 컴포넌트를 만든다. > yarn add styled-components - NewsItem 만들기 > article 이라는 객체를 props로 통째로 받아 와서 사용한다.

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
+        "http://newsapi.org/v2/top-headlines?country=kr&apiKey=097de04573d94611a53111fece7eaa25"
       );
       setData(response.data);
     } catch (e) {
