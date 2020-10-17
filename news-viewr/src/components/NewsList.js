dfb582595b78074f6306d2b5b2afbdf460b00615
develup8 <develup8@gmail.com>
2020-10-18 01:13:03 +0900

    API를 호출할 때 카테고리 지정하기 - NewsList 컴포넌트에서 현재 props로 받아온 category에 따라 카테고리를 지정하여 API를 요청한다. - category 값이 바뀔 때마다 뉴스를 새로 불러와야 하기 때문에 useEffect의 의존 배열(두 번째 파라미터로 설정하는 배열)에 category를 넣어 주어야 한다.

--- news-viewr/src/components/NewsList.js
+++ news-viewr/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -25,8 +25,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === "all" ? "" : `&category=${category}`;
         const response = await axios.get(
-          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=097de04573d94611a53111fece7eaa25"
+          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=097de04573d94611a53111fece7eaa25`
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   // 대기 중일 때
   if (loading) {
