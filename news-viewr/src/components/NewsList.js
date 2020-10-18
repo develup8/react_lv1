6f7eabf24f4ff9fc9848540f3cc2f19b19a45d46
develup8 <develup8@gmail.com>
2020-10-18 23:22:40 +0900

    usePromise 커스텀 Hook 만들기 - 컴포넌트에서 API 호출처럼 Promise를 사용해야 하는 경우 간결하게 코드를 작성할 수 있도록 커스텀 Hook을 만든다. - Promise의 대기 중, 완료 결과, 실패 결과에 대한 상태를 관리하며, usePromise의 의존 배열 deps를 파라미터로 받아온다. - 파라미터로 받아 온 deps 배열은 usePromise 내부에서 사용한 useEffect의 의존 배열로 설정된다.

--- news-viewr/src/components/NewsList.js
+++ news-viewr/src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,37 +18,27 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=097de04573d94611a53111fece7eaa25`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=097de04573d94611a53111fece7eaa25`
+    );
   }, [category]);
 
   // 대기 중일 때
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
-
-  // articels 값이 유요할 때
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  // reponse 값이 유요할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
